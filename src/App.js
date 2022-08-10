9b48d95dad122b160c571d88123b6013f1dc5b88
Shajwan <shajwannamiqsalh@gmail.com>
2022-08-11 00:07:15 +0300

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
- 
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route  } from "react-router-dom";
+
+import Header from "./components/Header"; 
+import Home  from "./components/Home";
+import About  from "./components/About";
+import Product from "./components/Product";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-     <div><h1 class="text-3xl  text-orange-500  font-bold underline">
-    Hello world!
-  </h1></div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="product" element={<Product />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+
+
+
+
+
+
+
+
+
+
+
+
+      
+    </>
   );
 }
 
