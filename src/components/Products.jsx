b57eae48efe4c7b323a12defaad2babc589f995a
Shajwan <shajwannamiqsalh@gmail.com>
2022-08-22 15:47:58 +0300

    update products page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
- 
+  import { FaSearch } from "react-icons/fa";
+
 
 const Products = () => {
   const [data, setData] = useState([]);
@@ -54,8 +55,8 @@ const Products = () => {
   const ShowProducts = () => {
     return (
       <>
-        <div className="container grid justify-items-center ">
-          <div className="my-10 grid grid-cols-5 grid-flow-row text-center border-b border-gray-200 text-gray-500">
+        <div className=" grid justify-items-center ">
+          <div className="my-10 grid grid-cols-6 grid-flow-row text-center border-b border-gray-200 text-gray-500">
             <button
               href="#page1"
               class="flex justify-center border-b-4 border-transparent hover:text-gray-800 hover:border-orange-400 py-4"
@@ -67,67 +68,68 @@ const Products = () => {
               href="#page1"
               class="flex justify-center border-b-4 border-transparent hover:text-gray-800 hover:border-orange-400 py-4"
             >
-              Dining Room
+              Office
             </button>
             <button
               href="#page2"
               class="flex justify-center border-b-4 border-transparent hover:text-gray-800 hover:border-orange-400 py-4"
             >
-              Kitchen
+              Living room
             </button>
 
             <button
               href="#page3"
               class="flex justify-center border-b-4 border-transparent hover:text-gray-800 hover:border-orange-400 py-4"
             >
-              Bed Room
+              Kitchen
             </button>
 
             <button
               href="#page4"
               class="flex justify-center border-b-4 border-transparent hover:text-gray-800 hover:border-orange-400 py-4"
             >
-              Kids Room
+              Bedroom
+            </button>
+            <button
+              href="#page4"
+              class="flex justify-center border-b-4 border-transparent hover:text-gray-800 hover:border-orange-400 py-4"
+            >
+              Dining
             </button>
           </div>
         </div>
 
-        <div className="px-4 py-16 mx-auto lg:ml-20 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 ">
+        <div className="px-4 py-16 mx-auto  sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 ">
           <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
             {filter.map((product) => {
               return (
                 <>
-                  <div class="overflow-hidden transition-shadow duration-300 bg-slate-900 rounded shadow-sm">
-                    <img
-                      className="object-cover w-full h-1/2"
-                      alt=""
-                      src={product.image}
-                    />
+                  <div class="overflow-hidden transition-shadow duration-300  rounded shadow-sm">
+                    <span className="group relative ">
+                      <img
+                        className="object-cover w-full  h-2/3"
+                        alt=""
+                        src={product.image}
+                      />
+                      <div class="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-[#fbfffe87] opacity-0 group-hover:h-full group-hover:opacity-100 duration-500">
+                        <a
+                          class="mt-5 px-8 py-3 rounded-full text-white  bg-slate-900 hover:bg-orange-400 duration-300"
+                          href="./product"
+                        >
+                          <FaSearch size={30} />
+                        </a>
+                      
+                      </div>
+                    </span>
 
-                    <div class="px-5 pb-5 mt-3 ">
-                      <a href="#">
-                        <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                    <div class="px-5 pb-5 h-1/3">
+                      <div class="mt-5 flex justify-between items-start">
+                        <span class="text-3xl font-bold text-gray-900 dark:text-gray-900">
+                          $599
+                        </span>
+                        <h5 class="float-left text-sm font-semibold tracking-tight text-orange-400 dark:text-gray-600">
                           {product.name}
                         </h5>
-                      </a>
-
-                      <div class="flex">
-                        <div className="row grid-cols-3 gap-3">
-                          <div className="float-left ">
-                            <span class="text-3xl font-bold text-gray-900 dark:text-white">
-                              ${product.price}
-                            </span>
-                          </div>
-
-                          <div className="float-right">
-                            <button
-                              href="#"
-                              class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                            >
-                              Buy Now
-                            </button>
-                          </div>
-                        </div>
                       </div>
                     </div>
                   </div>
@@ -145,25 +147,21 @@ const Products = () => {
 
 
   return (
-    <div>
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h1 className="flex justify-center mt-20 mb-20  text-2xl text-slate-600 font-bold">
-              Latest &nbsp;
-              <span className="border-b-4 border-orange-400">Products</span>
-              &nbsp; Furniture
-            </h1>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col">
-            {loading ? <Loading /> : <ShowProducts />}
-          </div>
+    <>
+      <div className="row">
+        <div className="col">
+          <h1 className="flex justify-center mt-20 mb-20  text-2xl text-slate-600 font-bold">
+            Latest &nbsp;
+            <span className="border-b-4 border-orange-400">Products</span>
+            &nbsp; Furniture
+          </h1>
         </div>
       </div>
-    </div>
+
+      <div className="row">
+        <div className="col">{loading ? <Loading /> : <ShowProducts />}</div>
+      </div>
+    </>
   );
 };
 
--- src/components/Test.js
+++ src/components/Test.js
@@ -3,47 +3,19 @@ import React from 'react'
 export default function Test() {
   return (
     <>
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="grid gap-8 lg:grid-cols-4  sm:max-w-sm sm:mx-auto lg:max-w-full">
-          <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
-            <img
-              src="https://images.pexels.com/photos/2408666/pexels-photo-2408666.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
-              className="object-cover w-full h-1/2"
-              alt=""
-            />
-            <div className="p-5 border border-t-0">
-              <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
-                <a
-                  href="/"
-                  className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
-                  aria-label="Category"
-                  title="traveling"
-                >
-                  traveling
-                </a>
-                <span className="text-gray-600">— 28 Dec 2020</span>
-              </p>
-              <a
-                href="/"
-                aria-label="Category"
-                title="Visit the East"
-                className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
-              >
-                Visit the East
-              </a>
-              <p className="mb-2 text-gray-700">
-                Sed ut perspiciatis unde omnis iste natus error sit sed quia
-                consequuntur magni voluptatem doloremque.
-              </p>
-              <a
-                href="/"
-                aria-label=""
-                className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-              >
-                Learn more
-              </a>
-            </div>
-          </div>
+      <div class="group relative w-96">
+        <img
+          class="w-full object-cover"
+          src="https://www.kindacode.com/wp-content/uploads/2022/06/t-shirt-example.png"
+        />
+        <div class="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-indigo-700 opacity-0 group-hover:h-full group-hover:opacity-100 duration-500">
+          <h1 class="text-2xl text-white">Fiction T-Shirt Store</h1>
+          <a
+            class="mt-5 px-8 py-3 rounded-full bg-amber-400 hover:bg-amber-600 duration-300"
+            href="#"
+          >
+            Continue Shopping
+          </a>
         </div>
       </div>
     </>
