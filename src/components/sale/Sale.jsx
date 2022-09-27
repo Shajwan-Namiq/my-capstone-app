a7e7507b9ce43262c32c4a7e8baa7d9b4d8a753d
Shajwan <shajwannamiqsalh@gmail.com>
2022-09-27 23:38:03 +0300

    sale page : adding movment to text (get 25% off) & adding the products

--- src/components/sale/Sale.jsx
+++ src/components/sale/Sale.jsx
@@ -1,27 +1,128 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import LoadingSpinner from "./LoadingSpinner";
 
 export default function Sale() {
+ 
+ 
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState(data);
+  const [loading, setLoading] = useState(false);
+
+  let componentMounted = true;
+
+  useEffect(() => {
+    const getProducts = async () => {
+      setLoading(true);
+
+      const response = await fetch(
+        "https://course-api.com/react-store-products"
+      );
+      if (componentMounted) {
+        setData(await response.clone().json());
+        setFilter(await response.json());
+        setLoading(false);
+      }
+
+      return () => {
+        componentMounted = false;
+      };
+    };
+
+    getProducts();
+  }, []);
+
+  const Loading = () => {
+    return (
+      <>
+        <LoadingSpinner />
+      </>
+    );
+  };
+
+   
+
+ 
+ 
+  const ShowProducts = () => {
+    return (
+      <>
+         
+
+        <div className="px-4 py-2 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 ">
+          <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
+            {filter.slice(0, 4).map((product) => {
+              return (
+                <div
+                  className=" overflow-hidden transition-shadow duration-300  rounded shadow-sm"
+                  key={product.id}
+                >
+                  <span className="group relative ">
+                    <img
+                      className="object-cover w-full  h-2/3"
+                      alt="productinfo"
+                      src={product.image}
+                    />
+                    <div className="absolute top-0 left-0 w-full h-0 flex flex-col justify-center items-center bg-[#fbfffe87] opacity-0 group-hover:h-full group-hover:opacity-100 duration-500">
+                      <Link
+                        className="mt-5 px-8 py-3 rounded-full text-white  bg-slate-900 hover:bg-orange-400 duration-300"
+                        to={`/products/${product.id}/`}
+                      >
+                        <FaSearch size={30} />
+                      </Link>
+                    </div>
+                  </span>
+
+                  <div className="px-2">
+                    <div className="mt-5 flex justify-between items-start">
+                      <h5 className="mt-2 float-left text-sm font-semibold tracking-tight text-gray-700 dark:text-gray-600">
+                        {product.name}
+                      </h5>
+                      <span className="text-3xl font-bold text-gray-900 dark:text-gray-900">
+                        ${product.price}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          
+        </div>
+      </>
+    );
+  };
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
   return (
     <>
       <div>
-        <section className="mx-5 bg-orange-400">
-          <div className="container  px-8 py-16   flex justify-center items-center  ">
-            <h2 className="text-3xl font-semibold tracking-tight text-white xl:text-4xl">
+        <section className="mx-5">
+          <h1 className=" text-3xl text-white font-bold bg-orange-400  px-8 py-10  flex justify-center items-center ">
+            <marquee behavior="alternate" direction="right">
               The Super plus sale | Get the 25% off
-            </h2>
-
-            <div className="mt-8 lg:mt-0">
-              <div className="flex flex-col space-y-3 sm:space-y-0 sm:flex-row sm:-mx-2"></div>
-            </div>
-          </div>
+            </marquee>
+          </h1>
         </section>
-
-
-
-
-
-        
       </div>
+
+      <br />
+
+      {loading ? <Loading /> : <ShowProducts />}
     </>
   );
 }
